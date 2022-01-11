3db7acb74276e8b82b33c3c66293b78600c8d8a1
nazaret <nazaret155@mail.ru>
2022-01-11 18:49:44 +0300

    added check role middleware to endpoint

--- server/routes/skillRouter.js
+++ server/routes/skillRouter.js
@@ -1,8 +1,10 @@
 const Router = require('express');
 const router = new Router();
 const skillController = require('../controllers/skillController.js');
+const checkRoleMiddleware = require('../middlewares/checkRoleMiddleware.js')
 
 router.get('/', skillController.getSkills);
+router.post('/', checkRoleMiddleware(2), skillController.createSkill)
 
 
 module.exports = router
