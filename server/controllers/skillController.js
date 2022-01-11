2ac3cd73b0740a3cf4cd6a3d7bb7459f7f2fd96e
nazaret <nazaret155@mail.ru>
2022-01-11 18:46:31 +0300

    added create method

--- server/controllers/skillController.js
+++ server/controllers/skillController.js
@@ -6,6 +6,12 @@ class SkillController {
         const skills = await Skill.findAll({attributes: ['id', 'name']});
         return res.json(skills)
     }
+
+    async createSkill(req, res, next) {
+        const {name} = req.body;
+        const skill = await Skill.create({name});
+        return res.json(skill);
+    }
 }
 
 module.exports = new SkillController()
