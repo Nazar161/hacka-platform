b11a2ffa776e8f117da5c82c4907cf2613ca0eb0
nazaret <nazaret155@mail.ru>
2022-01-13 21:43:52 +0300

    added cookie parser and cors settings

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const sequelize = require('./db')
 const models = require('./models/models.js')
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const router = require('./routes/index.js');
 const errorMiddleware = require('./middlewares/errorMiddleware.js')
 
@@ -10,7 +11,11 @@ const PORT = process.env.PORT || 5000;
 
 
 const app = express();
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
 app.use(express.json());
 app.use('/api', router);
 
