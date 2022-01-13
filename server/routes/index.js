dfbaf9469d5ba2a6a0dfc6825f2d5b09a5419b4c
nazaret <nazaret155@mail.ru>
2022-01-13 21:46:00 +0300

    added team router

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,12 @@ const router = new Router();
 const userRouter = require('./userRouter');
 const userInfoRouter = require('./userInfoRouter');
 const skillRouter = require('./skillRouter');
+const teamRouter = require('./teamRouter')
 
 router.use('/user', userRouter);
 router.use('/userInfo', userInfoRouter);
 router.use('/skill', skillRouter);
+router.use('/team', teamRouter);
+
 
 module.exports = router;
