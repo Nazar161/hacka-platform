7184b9c6660c669db4723450c34d079c2a6cef00
nazaret <nazaret155@mail.ru>
2022-03-12 18:27:40 +0300

    added event route

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,6 +7,8 @@ const skillRouter = require('./skillRouter');
 const teamRouter = require('./teamRouter');
 const caseRouter = require('./caseRouter');
 const vacancyRouter = require('./vacancyRouter');
+const eventRouter = require('./eventRouter');
+
 
 router.use('/user', userRouter);
 router.use('/userInfo', userInfoRouter);
@@ -14,6 +16,6 @@ router.use('/skill', skillRouter);
 router.use('/team', teamRouter);
 router.use('/case', caseRouter);
 router.use('/vacancy', vacancyRouter);
-
+router.use('/event', eventRouter);
 
 module.exports = router;
