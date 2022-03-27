a02262e9e31dc63001c3b8785be201199a6861fc
nazaret <nazaret155@mail.ru>
2022-03-27 10:09:12 +0300

    add header

---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import { Container } from '../../styledComponents';
+import Logo from './logo/Logo';
+import Navbar from './navbar/Navbar';
+import style from './header.module.css'
+
+const Header: FC = () => {
+    return (
+        <header className={style.header}>
+            <Container>
+                <div className={style.items}>
+                    <Logo/>
+                    <Navbar/>
+                </div>
+            </Container>
+        </header>
+    );
+};
+
+export default Header;
