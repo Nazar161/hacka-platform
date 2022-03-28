d5ada074b80467a7aba5096f1008812d6a307388
nazaret <nazaret155@mail.ru>
2022-03-28 22:20:46 +0300

    added new routes

--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -1,5 +1,10 @@
 import React from "react";
 import Home from "../pages/Home";
+import Account from "../pages/Account";
+import UserInfoUpd from "../pages/UserInfoUpd";
+import Registration from "../pages/Registration";
+import Teams from "../pages/Teams";
+import Team from "../pages/Team";
 
 export interface IRoute {
     element: React.ComponentType,
@@ -8,11 +13,14 @@ export interface IRoute {
 
 export enum publicRoutesNames {
     HOME = '',
-
+    REGISTRATION = 'registration',
 }
 
 export enum authRoutesNames {
-
+    ACCOUNT = 'account',
+    USER_INFO_UPDATE = 'userInfoUpd',
+    TEAMS = 'teams',
+    TEAM = 'teams/:teamId'
 }
 
 export enum captainRoutesNames {
@@ -29,10 +37,29 @@ export const publicRoutes: IRoute[] = [
         path: publicRoutesNames.HOME
 
     },
+    {
+        element: Registration,
+        path: publicRoutesNames.REGISTRATION
+    }
 ]
 
 export const authRoutes: IRoute[] = [
-
+    {
+        element: Account,
+        path: authRoutesNames.ACCOUNT
+    },
+    {
+        element: UserInfoUpd,
+        path: authRoutesNames.USER_INFO_UPDATE
+    },
+    {
+        element: Teams,
+        path: authRoutesNames.TEAMS
+    },
+    {
+        element: Team,
+        path: authRoutesNames.TEAM
+    }
 ]
 
 export const captainRoutes: IRoute[] = [
