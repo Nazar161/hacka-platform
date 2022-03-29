0b696e441fee27a46dde26f0325156689686a8f0
nazaret <nazaret155@mail.ru>
2022-03-29 23:42:50 +0300

    added vacancy pages

---
+++ client/src/pages/vacancy-page/Vacancies.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const Vacancies: FC = () => {
+    return (
+        <div>
+            Vacancies Page
+        </div>
+    );
+};
+
+export default Vacancies;
---
+++ client/src/pages/vacancy-page/VacancyItemPage.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const VacancyItemPage:FC = () => {
+    return (
+        <div>
+            VacancyItemPage
+        </div>
+    );
+};
+
+export default VacancyItemPage;
