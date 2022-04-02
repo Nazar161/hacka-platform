d5e89bfb37b89a7ae428aae8b3537312af138106
nazaret <nazaret155@mail.ru>
2022-04-02 21:54:49 +0300

    added new pages

---
+++ client/src/pages/case-page/CaseItemPage.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const CaseItemPage: FC = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CaseItemPage;
---
+++ client/src/pages/event-page/Events.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const Events: FC = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Events;
