5b048b904481c381d43c2ca63f987587c3a0708b
nazaret <nazaret155@mail.ru>
2022-04-04 22:22:08 +0300

    add User model (interface)

---
+++ client/src/models/User.ts
@@ -0,0 +1,10 @@
+export interface User{
+    email: string,
+    id: string,
+    isActivated: boolean,
+    name: string,
+    surname: string,
+    phone: string,
+    teamId: string,
+    roleId: number
+}
